0639b6d8ca5e5ff637707a981f33decf9620d0c6
YuriiB <burov.y1985@gmail.com>
2025-10-26 19:19:13 +0600

    STL1

---
+++ Counter_Num_STL1/Counter_Num_STL1/Counter_Num_STL1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <Windows.h>
+
+void CountChar(const std::string& a, std::map<char, int>& b) {
+    for (char c : a) {
+        b[c]++;       //Проверяет в мар наличие символа. Если символа нет создаем. Если есть добавляем +1
+    }
+
+}
+
+void sortVec (std::vector<std::pair<char, int>>& vec) {
+    sort(vec.begin(), vec.end(),
+        [](const std::pair<char, int>& a, const std::pair<char, int>& b) {
+            return a.second > b.second;
+        });
+}
+void Print(const std::vector<std::pair<char, int>> vec) {
+    std::cout << "Результат:" << std::endl;
+    for (const auto& pair : vec) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+}
+
+int main(){
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string Text;
+
+    // Ввод текста пользователем
+    std::cout << "Введите текст: ";
+    std::getline(std::cin, Text);
+
+    // Создаем map для подсчета частоты символов
+    std::map<char, int> charMap;
+
+    // Подсчитываем частоту каждого символа
+    CountChar(Text, charMap);
+
+    // Преобразуем map в vector пар для сортировки
+    std::vector<std::pair<char, int>> charVector(charMap.begin(), charMap.end());
+
+    // Сортируем по убыванию частоты
+    sortVec(charVector);
+
+    // Выводим результат
+    Print(charVector);
+
+    return 0;
+}
---
+++ Del_Doubl_STL1/Del_Doubl_STL1/Del_Doubl_STL1.cpp
@@ -0,0 +1,91 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <cstdlib>
+#include <algorithm>
+#include <unordered_set>
+
+//Создаем вектор и заполняем
+std::vector<int> CreateVec() {
+    int a{ 0 };
+    int size{ 0 };
+    std::vector<int> vec;
+    do
+    {
+        std::cout << "Введите положительное число количества элементов" << std::endl;
+        std::cin >> size;
+        if (size > 0) { 
+            std::cout << "Введите число" << std::endl;
+            std::cout << "IN" << std::endl;
+            break; }
+        else {
+            std::cout << "Неверно введено число" << std::endl;
+            Sleep(2000);
+            system("CLS");
+        }
+    } while (true);
+
+    do
+    {
+        std::cin >> a;
+        vec.push_back(a);
+       // std::cout << a << std::endl;
+    } while (vec.size() < size);
+    return vec;
+}
+// Создаем SET для отсеивания повторов
+void CopyVecToSet(const std::vector<int> vec, std::unordered_set <int>& set) {
+    
+    for (int num : vec) {
+        set.insert(num);
+    }
+    vec.~vector();
+}
+//Копируем из SET в вектор без повторов
+std::vector<int> CopySetToVec( const std::unordered_set <int> set) {
+   
+    std::vector<int> vec;
+    for (int num : set) {
+        vec.push_back(num);
+    }
+    return vec;
+}
+//Сортируем вектор
+void SortVec(std::vector<int>& vec) {
+    
+    sort (vec.begin(), vec.end(),
+         [](const int a, const int b) {
+             return a > b;
+         });
+}
+//Печать результата
+void Print(std::vector<int>& vec) {
+    std::cout << "\t" << std::endl;
+    std::cout << "Out:" << std::endl;
+    for (auto num : vec)
+    {
+        std::cout << num << std::endl;
+    }
+}
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    // Создаем вектор для хранения чисел
+    auto vec = CreateVec();
+
+    // Создаем SET для отсеивания повторов
+    std::unordered_set <int> set_;
+    CopyVecToSet(vec, set_);
+    
+    //Копируем из SET в вектор без повторов
+    vec = CopySetToVec(set_);
+
+    //Сортируем вектор
+    SortVec(vec);
+
+    //Печать результата
+    Print(vec);
+}
+
