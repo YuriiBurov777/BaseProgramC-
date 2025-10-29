3e2ae7b651c166879be9166ee74106983284f25e
YuriiB <burov.y1985@gmail.com>
2025-10-29 21:59:44 +0600

    STL2

---
+++ PrintConteiner_STL2/PrintConteiner/PrintConteiner.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+
+template<typename T>
+const void  Print(T cont) {
+    for (auto type : cont) {
+        std::cout << type << "  ";
+    }
+}
+
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    Print(test_set);
+    std::cout << std::endl;
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    Print(test_set);
+    std::cout << std::endl;
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    Print(test_set);
+    std::cout << std::endl;
+
+    std::set<int> test_set2 = { 1, 2, 3, 4 };
+    Print(test_set2);
+    std::cout << std::endl;
+
+    std::list<double> test_list2 = { 1.1, 2.2, 3.3, 4.4 };
+    Print(test_list2);
+    std::cout << std::endl;
+    
+    return 0;
+}
+
---
+++ UniqueVector_STL2/UniqueVector/UniqueVector.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+
+ void  Print(std::vector<int>& vec) {
+	for(int var : vec)
+	{
+		std::cout << var << " ";
+	}
+	std::cout << std::endl;
+}
+ template<typename T>
+ std::vector<T> Sort(std::vector<T>& vec) {
+	std::sort(vec.begin(), vec.end());
+	auto last = std::unique(vec.begin(), vec.end());
+	vec.erase(last, vec.end());
+	return vec;
+}
+
+
+
+ int main()
+ {
+	 std::vector<int> vec{ 1, 1, 2, 5, 6, 1, 2, 4 };
+	 Print(vec);
+	 Sort(vec);
+	 Print(vec);
+ }
+
