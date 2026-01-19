5786a1fbbb69a934760fb39b9628e6170dfe2e98
YuriiB <burov.y1985@gmail.com>
2026-01-19 19:11:36 +0600

    Multithreading_Sort_ForEarh

---
+++ Multithr_For_each/Multithr_For_each/Multithr_For_each.cpp
@@ -0,0 +1,44 @@
+﻿#include <future>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <thread>
+
+template<typename Iterator, typename V>
+void parallel_for_each(Iterator first, Iterator last, V LambdaFunc) {
+    size_t size = std::distance(first, last);
+    // Если мало элементов - обрабатываем последовательно
+    if (size <= 4) {
+        std::for_each(first, last, LambdaFunc);
+        return;
+    }
+    // Делим пополам
+    Iterator mid = first;
+    std::advance(mid, size / 2);
+
+    // Левая часть в отдельном потоке
+    auto left = std::async(parallel_for_each<Iterator, V>,first, mid, LambdaFunc);
+
+    // Правую часть обрабатываем здесь
+    parallel_for_each <Iterator, V> (mid, last, LambdaFunc);
+
+    // Ждем левую часть
+    left.get();
+}
+
+int main() {
+
+    std::vector<int> Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    // Умножаем все элементы на 3
+    parallel_for_each(Numbers.begin(), Numbers.end(),
+        [](int& x) { x *= 3; });
+
+    // Выводим результат
+    for (int n : Numbers) {
+        std::cout << n << " ";
+    }
+   
+
+    return 0;
+}
---
+++ Multithr_SortFuture/Multithr_SortFuture/Multithr_SortFuture.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <thread>
+#include <windows.h>
+
+// Асинхронный поиск индекса минимального элемента
+int find_min_index_async(const std::vector<int>& arr, int start) {
+    int min_index = start;
+    for (int i = start + 1; i < arr.size(); i++) {
+        if (arr[i] < arr[min_index]) {
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+// Сортировка выбором с асинхронным поиском минимума
+void select_sort(std::vector<int>& arr) {
+    for (int i = 0; i < arr.size() - 1; i++) {
+        // Запускаем асинхронный поиска
+        auto future = std::async(std::launch::async, find_min_index_async, std::ref(arr), i);
+
+        // Ждем результат
+        int min_index = future.get();
+
+        // Меняем местами
+        if (min_index != i) {
+            std::swap(arr[i], arr[min_index]);
+        }
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    //Создаем массив
+    std::vector<int> arr = { 644, 125, 12, 322, 101, 33, 90, 1 };
+
+    std::cout << "До сортировки: ";
+    for (int x : arr) std::cout << x << " ";
+    std::cout << std::endl;
+
+    //Запускаем сортировку
+    select_sort(arr);
+
+    std::cout << "После сортировки: ";
+    for (int x : arr) std::cout << x << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
