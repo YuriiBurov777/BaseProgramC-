25007fd0188e37c9be4556399ea7ad0154a7c107
YuriiB <burov.y1985@gmail.com>
2026-01-22 21:09:59 +0600

    TCP

---
+++ TCP_Client/TCP_Client/TCP_Client.cpp
@@ -0,0 +1,137 @@
+﻿// TCP_Client.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+class TCPClient {
+
+public:
+    TCPClient() : sock(INVALID_SOCKET), connected(false), result(0) {
+        result = WSAStartup(MAKEWORD(2, 2), &wsData);
+        if (result != 0) { std::cout << "WSAStartup failed" << std::endl; }
+
+    }
+
+    ~TCPClient() {
+        closesocket(sock);
+        sock = INVALID_SOCKET;
+        WSACleanup();
+    }
+
+    // Подключиться
+    bool connectTo(const char* ip = "127.0.0.1", int port = 8080) {
+        
+        //Задаем параметры сети в переменную hints
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+
+        //Задаем параметры сети в переменную hints
+        result = getaddrinfo("127.0.0.1", "12345", &hints, &addrResult);
+        if (result != 0) {
+            std::cout << "getaddrinfo failed" << result << std::endl;
+            /*WSACleanup();*/
+            return false;
+        }
+
+        //Создаем сокет
+        sock = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+        if (sock == INVALID_SOCKET) {
+            std::cout << "sock failed" << result << std::endl;
+           
+            //WSACleanup();
+            return false;
+        }
+
+        //Создаем соединение
+        result = connect(sock, addrResult->ai_addr, addrResult->ai_addrlen);
+        if (result == SOCKET_ERROR) {
+            std::cout << "connect failed" << result <<std::endl;
+            return false;
+        }
+
+        connected = true;
+        return true;
+    }
+
+    // Отправить данные
+    bool sendClient() {
+ 
+        if (!connected) return false;
+        else {
+           result = send(sock, sendBuffer, static_cast<int> (strlen(sendBuffer)), 0);
+           if (result == SOCKET_ERROR) {
+               std::cout << "send failed" << result << std::endl;
+               return false;
+           }
+           else { return true; }
+               
+        }
+    }
+
+    //Ставим сокет на прослушку
+    bool ShootD() {
+        result = shutdown(sock, SD_SEND);
+        if (result == SOCKET_ERROR) {
+            std::cout << "shutdown failed" << result << std::endl;
+            return false;
+        }
+
+    }
+
+    // Получить данные
+    std::string receive() {
+
+        if (!connected) {
+            return "Empty";
+        }
+        else {
+            do
+            {
+                ZeroMemory(recvBuffer, 10);
+                result = recv(sock, recvBuffer, static_cast<int> (strlen(recvBuffer)), 0);
+                if (result > 0) {
+                    std::cout << "Result" << result << std::endl;
+                    return recvBuffer;
+                }
+                else if (result == 0) {
+                    std::cout << "Connection closed" << result << std::endl;
+                    return "";
+                }
+                else  {
+                        std::cout << "recv failed" << result << std::endl;
+                        return "";
+                };
+            } while (result > 0);
+        }
+
+    }
+private:
+    SOCKET sock;
+    WSADATA wsData;
+    ADDRINFO hints;
+    ADDRINFO* addrResult = NULL;
+    const char* sendBuffer = "Hallo!";
+    char recvBuffer[10]{ "" };
+    bool connected;
+    int result;
+};
+
+int main() {
+
+    TCPClient client;
+   
+    if (!(client.connectTo())) { client.~TCPClient(); }
+    if (!(client.sendClient())) { client.~TCPClient(); }
+    if (!(client.ShootD())) { client.~TCPClient(); };
+    std::cout << client.receive()<<std::endl;
+ 
+
+    return 0;
+}
+
---
+++ TrhredTCPServer/TrhredTCPServer/TrhredTCPServer.cpp
@@ -0,0 +1,237 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <string>
+#include <atomic>
+
+#pragma comment(lib, "ws2_32.lib")
+
+class TCPServer {
+
+public:
+    TCPServer(int port = 12345) : port(port), isRunning(false), serverSocket(INVALID_SOCKET) {}
+
+    ~TCPServer() {
+        stop();
+    }
+
+    bool start() {
+        // Инициализация Winsock
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+            std::cerr << "Fail init Winsock" << std::endl;
+            return false;
+        }
+
+        // Создание сокета
+        serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (serverSocket == INVALID_SOCKET) {
+            std::cerr << "Creation error socket: " << WSAGetLastError() << std::endl;
+            WSACleanup();
+            return false;
+        }
+
+        // Настройка параметров сокета
+        int opt = 1;
+        if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR,
+            (char*)&opt, sizeof(opt)) == SOCKET_ERROR) {
+            std::cerr << "Socket configuration error: " << WSAGetLastError() << std::endl;
+            closesocket(serverSocket);
+            WSACleanup();
+            return false;
+        }
+
+        // Привязка сокета к адресу и порту
+      
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+        serverAddr.sin_port = htons(port);
+
+        if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+            std::cerr << "Socket binding error: " << WSAGetLastError() << std::endl;
+            closesocket(serverSocket);
+            WSACleanup();
+            return false;
+        }
+
+        // Прослушивание входящих соединений
+        if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+            std::cerr << "Fail listen: " << WSAGetLastError() << std::endl;
+            closesocket(serverSocket);
+            WSACleanup();
+            return false;
+        }
+
+        std::cout << "The server is running on the port " << port << std::endl;
+        isRunning = true;
+
+        // Основной цикл сервера
+        while (isRunning) {
+            sockaddr_in clientAddr;
+            int clientAddrSize = sizeof(clientAddr);
+
+            std::cout << "Waiting for connections..." << std::endl;
+
+            // Принятие входящего соединения
+            SOCKET clientSocket = accept(serverSocket,
+                (sockaddr*)&clientAddr,
+                &clientAddrSize);
+
+            if (clientSocket == INVALID_SOCKET) {
+                if (isRunning) {
+                    std::cerr << "Fail accept: " << WSAGetLastError() << std::endl;
+                }
+                continue;
+            }
+
+            // Вывод информации о подключившемся клиенте
+            char clientIP[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, INET_ADDRSTRLEN);
+
+            std::lock_guard<std::mutex> lock(consoleMutex);
+            std::cout << "New connection: " << clientIP << ":"
+                << ntohs(clientAddr.sin_port) << std::endl;
+
+            // Создание потока для обработки клиента
+            clientThreads.emplace_back([this, clientSocket]() {
+                this->handleClient(clientSocket);
+                });
+
+            // Очистка завершенных потоков
+            for (auto it = clientThreads.begin(); it != clientThreads.end();) {
+                if (it->joinable()) {
+                    it->join();
+                    it = clientThreads.erase(it);
+                }
+                else {
+                    ++it;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void stop() {
+        if (isRunning) {
+            isRunning = false;
+
+            // Закрытие серверного сокета для прерывания accept
+            if (serverSocket != INVALID_SOCKET) {
+                closesocket(serverSocket);
+                serverSocket = INVALID_SOCKET;
+            }
+
+            cleanup();
+            std::cout << "Server stoped" << std::endl;
+        }
+    }
+
+  private:
+        SOCKET serverSocket;
+        std::atomic<bool> isRunning;
+        std::vector<std::thread> clientThreads;
+        std::mutex consoleMutex;
+        int port;
+        sockaddr_in serverAddr;
+
+        void handleClient(SOCKET clientSocket) {
+            char buffer[1024];
+            int bytesReceived;
+
+            {
+                std::lock_guard<std::mutex> lock(consoleMutex);
+                std::cout << "The client is connected. Socket: " << clientSocket << std::endl;
+            }
+
+            // Получение данных от клиента
+            bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+
+            if (bytesReceived > 0) {
+                buffer[bytesReceived] = '\0';
+
+                {
+                    std::lock_guard<std::mutex> lock(consoleMutex);
+                    std::cout << "Received from the client " << clientSocket << ": "
+                        << buffer << std::endl;
+                }
+
+                // Формирование ответа
+                std::string response = "The server received: ";
+                response += buffer;
+                response += "\nServer response: Hello!";
+
+                // Отправка ответа клиенту
+                send(clientSocket, response.c_str(), response.length(), 0);
+
+                {
+                    std::lock_guard<std::mutex> lock(consoleMutex);
+                    std::cout << "Sent to the client " << clientSocket << ": "
+                        << response << std::endl;
+                }
+            }
+            else if (bytesReceived == 0) {
+                std::lock_guard<std::mutex> lock(consoleMutex);
+                std::cout << "The client has disconnected. Socket: " << clientSocket << std::endl;
+            }
+            else {
+                std::lock_guard<std::mutex> lock(consoleMutex);
+                std::cerr << "Error when receiving data. Code: "
+                    << WSAGetLastError() << std::endl;
+            }
+
+            // Закрытие сокета клиента
+            closesocket(clientSocket);
+        }
+
+        void cleanup() {
+            // Закрытие всех клиентских сокетов
+            for (auto& thread : clientThreads) {
+                if (thread.joinable()) {
+                    thread.join();
+                }
+            }
+            clientThreads.clear();
+
+            // Закрытие серверного сокета
+            if (serverSocket != INVALID_SOCKET) {
+                closesocket(serverSocket);
+                serverSocket = INVALID_SOCKET;
+            }
+
+            // Очистка Winsock
+            WSACleanup();
+        }
+
+};
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    TCPServer server(12345);
+
+    std::cout << "Launching a TCP server..." << std::endl;
+    std::cout << "Port: 12345" << std::endl;
+    std::cout << "To stop the server, press Enter..." << std::endl;
+
+    // Запуск сервера в отдельном потоке
+    std::thread serverThread([&server]() {
+        if (!server.start()) {
+            std::cerr << "Couldn't start the server" << std::endl;
+        }
+        });
+
+    // Ожидание нажатия Enter для остановки
+    std::cin.get();
+
+    server.stop();
+
+    if (serverThread.joinable()) {
+        serverThread.join();
+    }
+
+    return 0;
+}
