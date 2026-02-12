5083dc9e61999afb9fc1b461c287906cee770b87
YuriiB <burov.y1985@gmail.com>
2026-02-12 21:21:03 +0600

    Decorator

---
+++ Decorator/Decorator/Decorator.cpp
@@ -0,0 +1,105 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+
+    virtual ~Text() = default; // Виртуальный деструктор для корректного удаления через указатель на базовый класс
+};
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+
+    ~DecoratedText() override {
+        delete text_;
+    }
+
+protected:
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+
+    void render(const std::string& data) const override {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+
+    void render(const std::string& data) const override {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+// Класс для добавления параграфа
+class Paragraph : public DecoratedText {
+public:
+    explicit Paragraph(Text* text) : DecoratedText(text) {}
+
+    void render(const std::string& data) const override {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+// Класс для переворота строки
+class Reversed : public DecoratedText {
+public:
+    explicit Reversed(Text* text) : DecoratedText(text) {}
+
+    void render(const std::string& data) const override {
+        std::string reversed = data;
+        std::reverse(reversed.begin(), reversed.end());
+        text_->render(reversed);
+    }
+};
+// Класс для ссылки
+class Link : public DecoratedText {
+public:
+    explicit Link(Text* text) : DecoratedText(text) {}
+
+    // Новая сигнатура - два параметра
+    void render(const std::string& url, const std::string& text) const {
+        std::cout << "<a href=" << url << ">";
+        text_->render(text);
+        std::cout << "</a>";
+    }
+};
+
+int main() {
+
+    auto text_block = new ItalicText(new BoldText(new Text()));
+    text_block->render("Hello world");
+    std::cout << std::endl;
+    delete text_block;
+    // Пример с параграфом
+    auto text_block1 = new Paragraph(new Text());
+    text_block1->render("Hello world");
+    std::cout << std::endl;
+    delete text_block1;
+    // Пример с перевернутым текстом
+    auto text_block2 = new Reversed(new Text());
+    text_block2->render("Hello world");
+    std::cout << std::endl;
+    delete text_block2;
+    // Пример со ссылкой
+    auto text_block3 = new Link(new Text());
+    text_block3->render("netology.ru", "Hello world");
+    std::cout << std::endl;
+    delete text_block3;
+
+    return 0;
+}
---
+++ VeryHavyDB/VeryHavyDB/VeryHavyDB.cpp
@@ -0,0 +1,93 @@
+﻿
+#include <map>
+#include <iostream>
+#include <string>
+#include <memory>
+
+using namespace std;
+class VeryHeavyDatabase
+{
+public:
+	virtual std::string get_data(const std::string& key) {
+		return "Data:" + key;
+	}
+
+private:
+
+};
+class CachDB : public VeryHeavyDatabase
+{
+	unique_ptr<VeryHeavyDatabase> _realDB;
+	map<string, string> _cache;
+
+  public:
+	CachDB(unique_ptr<VeryHeavyDatabase> _real_DB) : _realDB(move(_real_DB)) {}
+	std::string get_data(const std::string& key) override {
+
+		if (_cache.find(key) == _cache.end()) {
+			cout << "Request to real DB" << endl;
+			_cache[key] = _realDB->get_data(key);
+		}
+		else {
+			cout << "Get from _cashe" << endl;
+		}
+		return _cache.at(key);
+	}
+
+};
+
+class OneShotDB : public VeryHeavyDatabase {
+private:
+    VeryHeavyDatabase* _realDB;
+    size_t _shots_left;
+    size_t _max_shots;
+    map<string, size_t> _access_count;
+
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : _realDB(real_object), _max_shots(shots), _shots_left(shots) {
+    }
+
+    std::string get_data(const std::string& key) override {
+        if (_shots_left == 0) {
+            return "error";
+        }
+
+        // Проверяем, сколько раз уже запрашивали этот ключ
+        if (_access_count.find(key) == _access_count.end()) {
+            _access_count[key] = 0;
+        }
+
+        if (_access_count[key] < _max_shots) {
+            _access_count[key]++;
+            _shots_left--;
+            return _realDB->get_data(key);
+        }
+        else {
+            return "error";
+        }
+    }
+};
+
+int main()
+{
+	auto Cache = make_unique<CachDB>(move(make_unique<VeryHeavyDatabase>()));
+
+	cout << Cache->get_data("Key1") << endl;
+	cout << Cache->get_data("Key2") << endl;
+	cout << Cache->get_data("Key1") << endl;
+	cout << Cache->get_data("Key3") << endl;
+	cout << Cache->get_data("Key3") << endl;
+
+    cout << "\nOneShotDB:" << endl;
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(&real_db, 2);
+
+    cout << limit_db.get_data("key") << endl;
+    cout << limit_db.get_data("key") << endl;
+    cout << limit_db.get_data("key") << endl;
+    cout << limit_db.get_data("other_key") << endl;
+
+
+	return 0;
+}
